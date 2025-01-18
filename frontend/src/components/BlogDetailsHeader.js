477ebf61ee387295b7638303c3261789069d9a88
RheaRoche <rocherhea01.14@gmail.com>
2025-01-18 18:04:43 +0530

    message

---
+++ frontend/src/components/BlogDetailsHeader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './BlogDetailsHeader.css'
+
+const BlogDetailsHeader = () => {
+  return (
+    <div className='BlogDetailsHeaderContent'> 
+      <h1>NEW DESIGN FOR FASHION</h1>
+      <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Amet odio vel imperdiet orci fermentum himenaeos. Venenatis lectus aenean proin ultricies suscipit fermentum phasellus. </p>
+    </div>
+  )
+}
+
+export default BlogDetailsHeader
